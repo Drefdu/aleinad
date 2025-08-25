5bdb264fc282f62daf97140ef5e3606a05422c70
Eduardo Arciniega <eduardo@mints.cloud>
2025-08-25 03:10:17 -0600

    new sections added to test view

---
+++ src/javascript/navbar.js
@@ -0,0 +1,21 @@
+
+let menuButton = document.getElementById('menu__buton');
+let navbar = document.getElementById('navbar');
+let toggleMenu = false;
+
+menuButton.addEventListener('click', (e) => {
+    toggleMenu = !toggleMenu;
+
+    if (toggleMenu) {
+        menuButton.classList.add('active');
+        navbar.classList.add('active');
+    } else {
+        menuButton.classList.remove('active');
+        navbar.classList.remove('active');
+    }
+
+})
+
+function displayMenu() {
+
+}
